6a9b374f0c6ff4d145d426d4c4b2c1ab9a67aaf9
Alexey <akurzov41@gmail.com>
2023-07-15 04:06:58 +0600

    Added Controller of initializers

---
+++ include/initializers/controller.hpp
@@ -0,0 +1,33 @@
+#ifndef SGE_INITIALIZERS_CONTROLLER_HPP
+#define SGE_INITIALIZERS_CONTROLLER_HPP
+
+#include <memory>
+#include <stack>
+#include <queue>
+
+#include "initializers/initializer.hpp"
+
+namespace SGE
+{
+  class InitializerController final
+  {
+   public:
+    using initializer_shared_t = std::shared_ptr< Initializer >;
+
+    InitializerController();
+    ~InitializerController();
+
+    void add(initializer_shared_t);
+    /**
+     * \brief Takes initializers from queue, and do initialise each one in right order
+     */
+    void init();
+    void quit();
+
+   private:
+    std::stack< initializer_shared_t > _inits_stack;
+    std::queue< initializer_shared_t > _inits_queue;
+  };
+}
+
+#endif
---
+++ src/initializers/controller.cpp
@@ -0,0 +1,43 @@
+#include "initializers/controller.hpp"
+
+SGE::InitializerController::InitializerController():
+  _inits_stack{},
+  _inits_queue{}
+{}
+
+SGE::InitializerController::~InitializerController()
+{
+  quit();
+}
+
+void SGE::InitializerController::add(initializer_shared_t initializer)
+{
+  _inits_queue.push(initializer);
+  _inits_stack.push(initializer);
+}
+
+void SGE::InitializerController::init()
+{
+  while (_inits_queue.empty())
+  {
+    auto initializer = _inits_queue.front();
+    _inits_queue.pop();
+    auto return_value = initializer->init();
+    if (return_value > 0)
+    {
+      quit();
+      _inits_queue = {};
+      return;
+    }
+  }
+}
+
+void SGE::InitializerController::quit()
+{
+  while (_inits_stack.empty())
+  {
+    auto initializer = _inits_stack.top();
+    _inits_stack.pop();
+    initializer->quit();
+  }
+}

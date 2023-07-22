4aeca4c8d86368c6e068238e0bc0a93f130278c9
Alexey <akurzov41@gmail.com>
2023-07-22 20:20:29 +0600

    Rainbow test program

--- src/SGE/main.cpp
+++ src/SGE/main.cpp
@@ -2,6 +2,8 @@
 #include <SDL3/SDL.h>
 #include "SGE/engine.hpp"
 
+#include <cmath>
+
 namespace
 {
   class TestScene final: public SGE::Scene
@@ -19,11 +21,26 @@ namespace
     void load() override
     {
       _backupClr = _engine.renderer().getDrawColor();
-      _engine.renderer().setDrawColor(255, 220, 80, SDL_ALPHA_OPAQUE);
     }
 
     void mloop() override
-    {}
+    {
+      constexpr double freq = .0005f;
+      static double i = 0;
+
+      SDL_Color clr = { 0, 0, 0, SDL_ALPHA_OPAQUE };
+
+      clr.r = std::sin(freq * i + 0) * 127 + 128;
+      clr.g = std::sin(freq * i + 2) * 127 + 128;
+      clr.b = std::sin(freq * i + 4) * 127 + 128;
+
+      if (i++ >= -1U)
+      {
+        i = 0;
+      }
+
+      _engine.renderer().setDrawColor(clr);
+    }
 
     void quit() override
     {
